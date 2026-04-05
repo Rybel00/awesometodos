d9b4ab902b05be7b7197fdc8cbe1e76864d18b5f
MallorcaB <bryleadammallorca@gmail.com>
2026-04-06 04:20:14 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,31 +1,33 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
 const { connectToMongoDB } = require("./database");
+const path = require("path");
+
 
 const app = express();
 app.use(express.json());
 
-// Serve React build
-app.use(express.static(path.join(dirname, "build")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(dirname, "build", "index.html"));
-});
+app.use(express.static(path.join(__dirname, "build")));
 
-// API routes
+
 const router = require("./routes");
 app.use("/api", router);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 
 const startServer = async () => {
-  const client = await connectToMongoDB();
-  if (!client) {
-    console.error("Failed to connect to MongoDB. Server is still running, but API may not work.");
-  }
-  app.listen(port, () => {
-    console.log(Server is listening on http://localhost:${port});
-  });
-};
-
+    try {
+        await connectToMongoDB();
+        app.listen(port, () => {
+            console.log(`Server is listening on http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+}
 startServer();
