a9e137d9885563784eea05ccbbcd757dab262b50
Bryle Adam E. Mallorca <bryleadammallorca@gmail.com>
2026-04-11 23:01:53 +0800

    fixed comments to improve readability

--- server/updateTodo.js
+++ server/updateTodo.js
@@ -1,23 +1,37 @@
-const { connectToMongoDB } = require("./database"); 
+const { connectToMongoDB } = require("./database");
 
+/**
+ * Updates a specific todo in the database
+ * - Finds a todo by its current text
+ * - Replaces it with new text
+ */
 const updateTodo = async () => {
   try {
+    // Connect to MongoDB and get the client
     const client = await connectToMongoDB();
-    const db = client.db("todosdb"); // database name
-    const todos = db.collection("todos"); // collection name
 
-    // Update a specific todo
-    const filter = { todo: "merci" }; // the todo you want to change
-    const update = { $set: { todo: "learn MongoDB today" } }; // new text
+    // Select the database and collection we’re working with
+    const db = client.db("todosdb");
+    const todosCollection = db.collection("todos");
 
+    // The todo we want to find and update
+    const filter = { todo: "merci" };
 
-    const result = await todos.updateOne(filter, update);
+    // What we want to change it to
+    const update = {
+      $set: { todo: "learn MongoDB today" },
+    };
 
+    // Perform the update operation
+    const result = await todosCollection.updateOne(filter, update);
+
+    // Log what happened
     console.log(`Matched ${result.matchedCount} document(s)`);
     console.log(`Modified ${result.modifiedCount} document(s)`);
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error("Error updating todo:", error);
   }
 };
 
+// Run the function
 updateTodo();
