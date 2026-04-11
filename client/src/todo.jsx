80c704e0b0de35a09afd86466cfab60a287de2f7
Bryle Adam E. Mallorca <bryleadammallorca@gmail.com>
2026-04-11 22:52:48 +0800

    fixed comments to improve readability

--- client/src/todo.jsx
+++ client/src/todo.jsx
@@ -1,53 +1,86 @@
 export default function Todo(props) {
-    const { todo, setTodos } = props;
+  // Pull out the todo item and the function to update the list
+  const { todo: todoItem, setTodos: updateTodoList } = props;
 
-    const updateTodo = async (todoId, todoStatus) => {
-        const res = await fetch(`/api/todos/${todoId}`, {
-            method: "PUT",
-            body: JSON.stringify({ status: todoStatus }),
-            headers: {
-                "Content-Type": "application/json"
-            },
+  /**
+   * Toggle the status of a todo (completed ↔ not completed)
+   */
+  const handleToggleTodoStatus = async (todoId, currentStatus) => {
+    const response = await fetch(`/api/todos/${todoId}`, {
+      method: "PUT",
+      body: JSON.stringify({ status: currentStatus }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const result = await response.json();
+
+    // If the update worked on the backend, reflect the change in the UI
+    if (result.acknowledged) {
+      updateTodoList((currentTodos) => {
+        return currentTodos.map((existingTodo) => {
+          // Flip the status of the matching todo
+          if (existingTodo._id === todoId) {
+            return {
+              ...existingTodo,
+              status: !existingTodo.status,
+            };
+          }
+          return existingTodo;
         });
-        const json = await res.json();
-        if (json.acknowledged) {
-            setTodos(currentTodos => {
-                return currentTodos.map((currentTodo) => {
-                    if (currentTodo._id === todoId) {
-                        return { ...currentTodo, status: !currentTodo.status };
-                    }
-                    return currentTodo;
-                });
-            });
-        };
-    };
-    const deleteTodo = async (todoId) => {
-        const res = await fetch(`/api/todos/${todoId}`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json"
-            },
-        });
-        const json = await res.json();
-        if (json.acknowledged) {
-            setTodos(currentTodos => {
-                return currentTodos.filter((currentTodo) => currentTodo._id !== todoId);
-            });
-        };
-    };
-    return (
-        <div className="todo">
-            <p className={todo.status ? "todo--completed" : ""}>{todo.todo}</p>
-            <div className="mutations">
-                <button className="todo__status"
-                    onClick={() => updateTodo(todo._id, todo.status)}>
-                    {(todo.status) ? "☑" : "☐"}
-                </button>
-                <button className="todo__delete"
-                    onClick={() => deleteTodo(todo._id)}>
-                    🗑
-                </button>
-            </div>
-        </div>
-    );
+      });
+    }
+  };
+
+  /**
+   * Remove a todo from both the backend and the UI
+   */
+  const handleDeleteTodo = async (todoId) => {
+    const response = await fetch(`/api/todos/${todoId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const result = await response.json();
+
+    // If deletion was successful, remove it from the list in the UI
+    if (result.acknowledged) {
+      updateTodoList((currentTodos) => {
+        return currentTodos.filter(
+          (existingTodo) => existingTodo._id !== todoId
+        );
+      });
+    }
+  };
+
+  return (
+    <div className="todo">
+      {/* Show the todo text, and style it differently if it's completed */}
+      <p className={todoItem.status ? "todo--completed" : ""}>
+        {todoItem.todo}
+      </p>
+
+      {/* Action buttons for updating or deleting the todo */}
+      <div className="mutations">
+        <button
+          className="todo__status"
+          onClick={() =>
+            handleToggleTodoStatus(todoItem._id, todoItem.status)
+          }
+        >
+          {todoItem.status ? "☑" : "☐"}
+        </button>
+
+        <button
+          className="todo__delete"
+          onClick={() => handleDeleteTodo(todoItem._id)}
+        >
+          🗑
+        </button>
+      </div>
+    </div>
+  );
 }
