17c39518504e2603bf27e8040b50ff327bbeab2b
Bryle Adam E. Mallorca <bryleadammallorca@gmail.com>
2026-04-11 22:50:13 +0800

    fixed comments to improve readability

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Grab the HTML element where our React app will live
+const rootElement = document.getElementById("root");
+
+// Create a root container so React knows where to render the app
+const reactRoot = ReactDOM.createRoot(rootElement);
+
+// Render the App component into the page
+// React.StrictMode is turned on to help catch potential issues during development
+reactRoot.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
